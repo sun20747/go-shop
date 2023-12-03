a2609b07f11e7045716ea432c121b2dd6452639a
DESKTOP-4QI2T92\Window 10 <sunny20747@gmail.com>
2023-12-03 21:30:18 +0700

    set env

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/go-shop/config"
+)
+
 func main() {
+	ctx := context.Background()
+	_ = ctx
+
+	// Initialize config
+
+	cfg := config.LoadConfig(func() string {
+		if len(os.Args) < 2 {
+			log.Fatal("Error .env path required")
+		}
+		return os.Args[1]
+	}())
+
+	log.Println(cfg)
 
 }
